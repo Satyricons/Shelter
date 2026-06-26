87075fbda07033f4560b9ecb582b17d74caff1d3
Егор <satyricons@yandex.ru>
2026-06-26 21:42:41 +0300

    JS014+asynh (Fri, Jun 26, 2026 21:42)

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
-// 1. Отправляем запрос к файлу
+let list = []
+
 fetch('data.json')
-  // 2. Обрабатываем ответ от сервера
   .then(response => {
-    // Проверяем, был ли запрос успешным
-    if (!response.ok) {
-      // Если нет (например, файл не найден), выбрасываем ошибку
-      throw new Error(`Ошибка при загрузке файла: ${response.status}`)
-    }
-    // Если все хорошо, преобразуем ответ в JSON-объект
+    if (!response.ok) throw new Error(`Ошибка: ${response.status}`)
     return response.json()
   })
-  // 3. Работаем с полученными данными
-  .then(list => {
-    // В этой переменной 'data' теперь находится обычный JavaScript-массив с объектами
+  .then(data => {
+    list = data
 
     //Формируем весь список живности
-
     for (let i = 0; i < list.length; i++) {
       document.querySelector('.slides').insertAdjacentHTML(
         'beforeend',
@@ -26,21 +19,27 @@ fetch('data.json')
                 </div>`
       )
     }
+  })
+  .catch(error => {
+    console.error('Ошибка:', error)
+  })
 
-    buttonStatus('inactive')
-    function buttonStatus (status) {
-      document.querySelector('.prev').setAttribute('id', status)
-    }
 
-    //Инициализация
 
-    if (
-      !document.querySelector('.container_modal_menu') &
-      (window.innerWidth < 768)
-    ) {
-      document.body.insertAdjacentHTML(
-        'beforeend',
-        `<div class="container_modal_menu">
+buttonStatus('inactive')
+function buttonStatus (status) {
+  document.querySelector('.prev').setAttribute('id', status)
+}
+
+//Инициализация
+
+if (
+  !document.querySelector('.container_modal_menu') &
+  (window.innerWidth < 768)
+) {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `<div class="container_modal_menu">
     <nav class="mob_nav-menu_pets">
           <ul>
             <li id="active_a_pets"><a href="./index.html#about">About the shelter</a></li>
@@ -50,136 +49,116 @@ fetch('data.json')
           </ul>
         </nav>    
     </div>`
-      )
-      document
-        .querySelector('.container_modal_menu')
-        .addEventListener('click', closeBurger)
-    } else if (window.innerWidth < 768) {
-      document
-        .querySelector('.container_modal_menu')
-        .setAttribute('id', 'inactive')
-    }
+  )
+  document
+    .querySelector('.container_modal_menu')
+    .addEventListener('click', closeBurger)
+} else if (window.innerWidth < 768) {
+  document.querySelector('.container_modal_menu').setAttribute('id', 'inactive')
+}
 
-    //Модальное окно при нажатии кнопки
+//Модальное окно при нажатии кнопки
+function showPet (id) {
+  console.log(list)
 
-    function showPet (id) {
-      document.body.insertAdjacentHTML(
-        'beforeend',
-        `<div class="container_modal">
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    `<div class="container_modal">
     <div class="modal">
     <img src=${list[id].img} alt="">
     </div>
     <img onclick=closePet() class="close_modal" src="./img/modal_close_button.png">
     </div>`
-      )
-    }
+  )
+}
 
-    function closePet () {
-      document.querySelector('.container_modal').remove()
-    }
+function closePet () {
+  document.querySelector('.container_modal').remove()
+}
 
-    //Сдвиг слайдеров
+//Сдвиг слайдеров
 
-    let page = 0
-    let pix = 0
-    document.querySelector('.next').addEventListener('click', next)
-    document.querySelector('.prev').addEventListener('click', prev)
-    document.querySelector('.burger').addEventListener('click', openBurger)
+let page = 0
+let pix = 0
+document.querySelector('.next').addEventListener('click', next)
+document.querySelector('.prev').addEventListener('click', prev)
+document.querySelector('.burger').addEventListener('click', openBurger)
 
-    function next () {
-      if (window.innerWidth >= 1280) {
-        pix = -(
-          document.querySelector('.slides').parentElement.clientWidth + 90
-        )
-      } else if (window.innerWidth >= 768) {
-        pix = -(
-          document.querySelector('.slides').parentElement.clientWidth + 40
-        )
-      } else {
-        pix = -(
-          document.querySelector('.slides').parentElement.clientWidth + 40
-        )
-      }
+function next () {
+  if (window.innerWidth >= 1280) {
+    pix = -(document.querySelector('.slides').parentElement.clientWidth + 90)
+  } else if (window.innerWidth >= 768) {
+    pix = -(document.querySelector('.slides').parentElement.clientWidth + 40)
+  } else {
+    pix = -(document.querySelector('.slides').parentElement.clientWidth + 40)
+  }
 
-      let countPages =
-        window.innerWidth >= 1280
-          ? list.length / 3
-          : window.innerWidth >= 768
-          ? list.length / 2
-          : window.innerWidth >= 320
-          ? list.length / 1
-          : 1
+  let countPages =
+    window.innerWidth >= 1280
+      ? list.length / 3
+      : window.innerWidth >= 768
+      ? list.length / 2
+      : window.innerWidth >= 320
+      ? list.length / 1
+      : 1
 
-      if (page + 1 < countPages) {
-        page++
-        buttonStatus('active')
-        document.querySelector('.slides').style.transform = `translate(${
-          pix * page
-        }px)`
-      } else {
-        page = 0
-        document.querySelector('.slides').style.transform = `translate(0px)`
-        buttonStatus('inactive')
-      }
-    }
+  if (page + 1 < countPages) {
+    page++
+    buttonStatus('active')
+    document.querySelector('.slides').style.transform = `translate(${
+      pix * page
+    }px)`
+  } else {
+    page = 0
+    document.querySelector('.slides').style.transform = `translate(0px)`
+    buttonStatus('inactive')
+  }
+}
 
-    function prev () {
-      if (window.innerWidth >= 1280) {
-        pix = -(
-          document.querySelector('.slides').parentElement.clientWidth + 90
-        )
-      } else if (window.innerWidth >= 768) {
-        pix = -(
-          document.querySelector('.slides').parentElement.clientWidth + 40
-        )
-      } else {
-        pix = -(
-          document.querySelector('.slides').parentElement.clientWidth + 40
-        )
-      }
+function prev () {
+  if (window.innerWidth >= 1280) {
+    pix = -(document.querySelector('.slides').parentElement.clientWidth + 90)
+  } else if (window.innerWidth >= 768) {
+    pix = -(document.querySelector('.slides').parentElement.clientWidth + 40)
+  } else {
+    pix = -(document.querySelector('.slides').parentElement.clientWidth + 40)
+  }
+  if (page > 0) {
+    page--
+    document.querySelector('.slides').style.transform = `translate(${
+      pix * page
+    }px)`
+    if (page < 1) buttonStatus('inactive')
+  }
+}
 
-      if (page > 0) {
-        page--
-        document.querySelector('.slides').style.transform = `translate(${
-          pix * page
-        }px)`
-        if (page < 1) buttonStatus('inactive')
-      }
-    }
+// Отследить событие изменения окна
+let lastWidth = window.innerWidth
+window.addEventListener('resize', () => {
+  const currentWidth = window.innerWidth
+  if (currentWidth !== lastWidth) {
+    console.log(`Ширина изменена! Текущая ширина: ${currentWidth}px`)
+    document.querySelector('.slides').style.transform = `translate(0px)`
+    page = 0
+    buttonStatus('inactive')
+    lastWidth = currentWidth // обновляем сохраненное значение
+  }
+})
 
-    // отследить событие изменения окна
-
-    let lastWidth = window.innerWidth
-
-    window.addEventListener('resize', () => {
-      const currentWidth = window.innerWidth
-
-      if (currentWidth !== lastWidth) {
-        console.log(`Ширина изменена! Текущая ширина: ${currentWidth}px`)
-        document.querySelector('.slides').style.transform = `translate(0px)`
-        page = 0
-        buttonStatus('inactive')
-        lastWidth = currentWidth // обновляем сохраненное значение
-      }
-    })
-
-    function openBurger () {
-      !document.getElementById('active_menu')
-        ? document
-            .querySelector('.container_modal_menu')
-            .setAttribute('id', 'active_menu')
-        : document
-            .querySelector('.container_modal_menu')
-            .setAttribute('id', 'inactive_menu')      
-    }
-
-    function closeBurger () {
-      document
+//Открываем и закрываем мобильное меню
+function openBurger () {
+  !document.getElementById('active_menu')
+    ? document
+        .querySelector('.container_modal_menu')
+        .setAttribute('id', 'active_menu')
+    : document
         .querySelector('.container_modal_menu')
         .setAttribute('id', 'inactive_menu')
-    }
-  })
-  // 4. Обрабатываем возможные ошибки (например, файл не найден)
-  .catch(error => {
-    console.error('Произошла ошибка:', error)
-  })
+}
+
+//Если нажать на мобильное окно, то оно закроется
+function closeBurger () {
+  document
+    .querySelector('.container_modal_menu')
+    .setAttribute('id', 'inactive_menu')
+}
